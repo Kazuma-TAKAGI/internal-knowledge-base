51d2cfbe9d8854325e260a2ef98059d2f4f783d0
takokiji NotePC <takokiji98@gmail.com>
2026-01-24 18:05:39 +0900

    first commit

---
+++ src/main/java/com/internalknowledgebase/InternalKnowledgeBaseApplication.java
@@ -0,0 +1,13 @@
+package com.internalknowledgebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternalKnowledgeBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternalKnowledgeBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/internalknowledgebase/InternalKnowledgeBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internalknowledgebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternalKnowledgeBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
